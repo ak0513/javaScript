66a260031c7a9efe3250c8ba8a5d21b3638a7c95
ak0513 <minimar0812@gmail.com>
2022-09-02 13:08:26 +0900

    removeClass

--- common.js
+++
@@ -1,9 +0,0 @@
-var ui = {
-    // 형제 요소 찾기
-    siblings: function(ele) {
-		return [...document.querySelector(ele).parentNode.children].filter((child) =>
-			child !== document.querySelector(ele)
-		)
-	},
-    randomNum2: new Date().setDate(new Date().getDate())
-}
