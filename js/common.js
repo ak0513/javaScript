808d3747ad4ee70bc17d6d2673d1510823d598cc
ak0513 <minimar0812@gmail.com>
2022-10-04 16:20:06 +0900

    userAgent

--- js/common.js
+++ js/common.js
@@ -2,40 +2,36 @@ document.addEventListener('DOMContentLoaded', function() {
 	setAttrRandomNum('link[rel="stylesheet"]', 'href');
 	setAttrRandomNum('script[src]', 'src');
 
-
-})
-document.addEventListener('DOMContentLoaded', function() {
-	var body = document.querySelector('body');
-	var ua = navigator.userAgent.toLowerCase();
-	
-	// 모바일 체크
-	var userAgent = {
-		BlackBerry: function () {return ua.match(/blackberry/i) === null ? false : true;},
-		Android: function () {return ua.match(/android/i) === null ? false : true;},
-		iOS: function () {return ua.match(/iphone|ipad|ipod/i) === null ? false : true;},
-		iPhone: function () {return ua.match(/iphone/i) === null ? false : true;},
-		iPad: function () {return ua.match(/ipad/i) === null ? false : true;},
-		Windows: function () {return ua.match(/windows/i) === null ? false : true;},
-		edge: function () {return ua.match(/edge|edg/i) === null ? false : true;},
-		opera: function () {return ua.match(/opr/i) === null ? false : true;},
-		chrome: function () {return ua.match(/chrome/i) === null ? false : true;},
-		msie: function () {return ua.match(/trident/i) === null ? false : true;},
-		firefox: function () {return ua.match(/firefox/i) === null ? false : true;},
-		safari: function () {return ua.match(/safari/i) === null ? false : true;},
-		any: function () {return (userAgent.Android() || userAgent.BlackBerry() || userAgent.iOS() || userAgent.Opera() || userAgent.Windows())},
-		isMobile: function () {return ua.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i) === null ? false : true;},
-	}
-	
-	var deviceInfo = {
-		os: null,
-		device: null,
-		browser : null,
-		mobile : userAgent.isMobile(),
-	}
+	setDeviceInfo();
+	setBodyClass();
 })
 
+var ua = navigator.userAgent.toLowerCase();
 
+// 모바일 체크
+var userAgent = {
+	BlackBerry: function () {return ua.match(/blackberry/i) === null ? false : true;},
+	Android: function () {return ua.match(/android/i) === null ? false : true;},
+	iOS: function () {return ua.match(/iphone|ipad|ipod/i) === null ? false : true;},
+	iPhone: function () {return ua.match(/iphone/i) === null ? false : true;},
+	iPad: function () {return ua.match(/ipad/i) === null ? false : true;},
+	Windows: function () {return ua.match(/windows/i) === null ? false : true;},
+	edge: function () {return ua.match(/edge|edg/i) === null ? false : true;},
+	opera: function () {return ua.match(/opr/i) === null ? false : true;},
+	chrome: function () {return ua.match(/chrome/i) === null ? false : true;},
+	msie: function () {return ua.match(/trident/i) === null ? false : true;},
+	firefox: function () {return ua.match(/firefox/i) === null ? false : true;},
+	safari: function () {return ua.match(/safari/i) === null ? false : true;},
+	any: function () {return (userAgent.Android() || userAgent.BlackBerry() || userAgent.iOS() || userAgent.Opera() || userAgent.Windows())},
+	isMobile: function () {return ua.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i) === null ? false : true;},
+}
 
+var deviceInfo = {
+	os: null,
+	device: null,
+	browser : null,
+	mobile : userAgent.isMobile(),
+}
 
 
 
@@ -80,11 +76,10 @@ function setBodyClass() {
 	// pc mobile 체크
 	var setPlatform = deviceInfo.mobile ? 'mobile' : 'pc';
 	var bodyClass = setPlatform + ' ' + deviceInfo.os + ' ' + deviceInfo.browser + ' ' + deviceInfo.device;
-	body.setAttribute('class', bodyClass)
+	document.querySelector('body').setAttribute('class', bodyClass)
 }
 
-setDeviceInfo(); // deviceInfo 세팅
-setBodyClass(); // body에 device별 클래스 추가
+
 // class 삭제
 function removeClass(ele, className) {
 	for(var i = 0; i < ele.length; i++) {
@@ -116,4 +111,11 @@ function setAttrRandomNum(ele, attr) {
 		eleAttr = eleAttr + '?' + getRandomNum();
 		ele[i].setAttribute(attr, eleAttr)
 	}
+}
+
+// url파라미터 값 구하기
+function getUrlParam(param) {
+	var urlParams = new URL(location.href).searchParams;
+	var name = urlParams.get(param);
+	return name;
 }
