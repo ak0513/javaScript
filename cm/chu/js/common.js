ba89e987444c02e814ffcb6abed2eba1589eebe3
ak0513 <minimar0812@gmail.com>
2022-11-11 14:09:22 +0900

    chu

--- cm/chu/js/common.js
+++ cm/chu/js/common.js
@@ -2,114 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	setAttrRandomNum(document.querySelectorAll('link[rel="stylesheet"]'), 'href');
 	setAttrRandomNum(document.querySelectorAll('script[src]'), 'src');
 
-	var eleFocusTags = 'input:not([tabindex]), button:not([tabindex]), a:not([tabindex]), select:not([tabindex]), textarea:not([tabindex])';
-	var eleTabindex = '[tabindex="0"]';
-	var eleTabindexM = '[tabindex="-1"]';
-	// 포커스 비활성화(접근성)
-	function accessDisable(eleDisable, module) {
-		eleDisable.forEach(function(ele) {
-			var eleTabindexMChild = ele.querySelectorAll(eleTabindexM);
-			var eleFocusTagsChild = ele.querySelectorAll(eleFocusTags);
-			var eleTabindexChild = ele.querySelectorAll(eleTabindex);
-			ele.setAttribute('aria-hidden', true);
-			ele.classList.add('is-disable-'+module+'-ariaHidden');
-			eleTabindexMChild.forEach(function(ele) {
-				ele.classList.add('is-disable-'+module+'-fixed')
-			});
-			eleFocusTagsChild.forEach(function(ele) {
-				ele.setAttribute('tabindex', '-1');
-				ele.classList.add('is-disable-'+module+'-tags')
-			});
-			eleTabindexChild.forEach(function(ele) {
-				ele.setAttribute('tabindex', '-1');
-				ele.classList.add('is-disable-'+module+'-tabindex')
-			});
-		});
-	}
-
-	// 포커스 활성화(접근성)
-	function accessEnable(eleEnable, module){
-		eleEnable.forEach(function(ele) {
-			
-			var eleTabindexMChild = ele.querySelectorAll('.is-disable-'+module+'-fixed');
-			var eleFocusTagsChild = ele.querySelectorAll('.is-disable-'+module+'-tags');
-			var eleTabindexChild = ele.querySelectorAll('.is-disable-'+module+'-tabindex');
-			ele.setAttribute('aria-hidden', false);
-			ele.classList.remove('is-disable-'+module+'-ariaHidden');
-			eleTabindexMChild.forEach(function(ele) {
-				ele.classList.remove('is-disable-'+module+'-fixed')
-			});
-			eleFocusTagsChild.forEach(function(ele) {
-				ele.removeAttribute('tabindex');
-				ele.classList.remove('is-disable-'+module+'-tags')
-			});
-			eleTabindexChild.forEach(function(ele) {
-				ele.setAttribute('tabindex', '0');
-				ele.classList.remove('is-disable-'+module+'-tabindex')
-			});
-		});
-	}
-
-
-	// class 삭제
-	function removeClass(ele, className) {
-		for(var i = 0; i < ele.length; i++) {
-			ele[i].classList.remove(className)
-		}
-	}
-
-	// 형제요소 찾기
-	function siblings(ele) {
-		if(typeof(ele) === 'string') {
-			ele = document.querySelector(ele)
-		}
-		var siblings = [...ele.parentNode.children].filter((child) =>
-			child !== ele
-		)
-		return siblings
-	}
-
-	// 랜덤숫자 얻기
-	function getRandomNum() {
-		return new Date().setDate(new Date().getDate())
-	}
-
-	// attr에 랜덤함수
-	function setAttrRandomNum(ele, attr) {
-		for(var i =0; i < ele.length; i++) {
-			var eleAttr = ele[i].getAttribute(attr)
-			eleAttr = eleAttr + '?' + getRandomNum();
-			ele[i].setAttribute(attr, eleAttr)
-		}
-	}
-
-	// attr 세팅
-	function setAttr(ele, attr, value) {
-		ele.forEach(function(ele) {
-			ele.setAttribute(attr, value)
-		})
-	}
-
-	// url파라미터 값 구하기
-	function getUrlParam(param) {
-		var urlParams = new URL(location.href).searchParams;
-		var name = urlParams.get(param);
-		return name;
-	}
-
-	// 가장 가까운 부모 찾기
-	function closest(ele, selector) {
-		var matchesSelector = ele.matches || ele.webkitMatchesSelector || ele.mozMatchesSelector || ele.msMatchesSelector;
-		while (ele) {
-			if (matchesSelector.call(ele, selector)) {
-				return ele;
-			} else {
-				ele = ele.parentElement;
-			}
-		}
-		return null;
-	}
-
 	var sideMenuLink = document.querySelectorAll('.menu-link.has-sub');
 	var sideMenuItem = document.querySelectorAll('.menu-item');
 	var sideMenuDepth2 = document.querySelectorAll('.menu-depth2');
@@ -184,3 +76,140 @@ document.addEventListener('DOMContentLoaded', function() {
 		return eleCloneHeight
 	}
 })
+var eleFocusTags = 'input:not([tabindex]), button:not([tabindex]), a:not([tabindex]), select:not([tabindex]), textarea:not([tabindex])';
+var eleTabindex = '[tabindex="0"]';
+var eleTabindexM = '[tabindex="-1"]';
+// 포커스 비활성화(접근성)
+function accessDisable(eleDisable, module) {
+	eleDisable.forEach(function(ele) {
+		var eleTabindexMChild = ele.querySelectorAll(eleTabindexM);
+		var eleFocusTagsChild = ele.querySelectorAll(eleFocusTags);
+		var eleTabindexChild = ele.querySelectorAll(eleTabindex);
+		ele.setAttribute('aria-hidden', true);
+		ele.classList.add('is-disable-'+module+'-ariaHidden');
+		eleTabindexMChild.forEach(function(ele) {
+			ele.classList.add('is-disable-'+module+'-fixed')
+		});
+		eleFocusTagsChild.forEach(function(ele) {
+			ele.setAttribute('tabindex', '-1');
+			ele.classList.add('is-disable-'+module+'-tags')
+		});
+		eleTabindexChild.forEach(function(ele) {
+			ele.setAttribute('tabindex', '-1');
+			ele.classList.add('is-disable-'+module+'-tabindex')
+		});
+	});
+}
+
+// 포커스 활성화(접근성)
+function accessEnable(eleEnable, module){
+	eleEnable.forEach(function(ele) {
+		
+		var eleTabindexMChild = ele.querySelectorAll('.is-disable-'+module+'-fixed');
+		var eleFocusTagsChild = ele.querySelectorAll('.is-disable-'+module+'-tags');
+		var eleTabindexChild = ele.querySelectorAll('.is-disable-'+module+'-tabindex');
+		ele.setAttribute('aria-hidden', false);
+		ele.classList.remove('is-disable-'+module+'-ariaHidden');
+		eleTabindexMChild.forEach(function(ele) {
+			ele.classList.remove('is-disable-'+module+'-fixed')
+		});
+		eleFocusTagsChild.forEach(function(ele) {
+			ele.removeAttribute('tabindex');
+			ele.classList.remove('is-disable-'+module+'-tags')
+		});
+		eleTabindexChild.forEach(function(ele) {
+			ele.setAttribute('tabindex', '0');
+			ele.classList.remove('is-disable-'+module+'-tabindex')
+		});
+	});
+}
+
+
+// class 삭제
+function removeClass(ele, className) {
+	for(var i = 0; i < ele.length; i++) {
+		ele[i].classList.remove(className)
+	}
+}
+
+// 형제요소 찾기
+function siblings(ele) {
+	if(typeof(ele) === 'string') {
+		ele = document.querySelector(ele)
+	}
+	var siblings = [...ele.parentNode.children].filter((child) =>
+		child !== ele
+	)
+	return siblings
+}
+
+// 랜덤숫자 얻기
+function getRandomNum() {
+	return new Date().setDate(new Date().getDate())
+}
+
+// attr에 랜덤함수
+function setAttrRandomNum(ele, attr) {
+	for(var i =0; i < ele.length; i++) {
+		var eleAttr = ele[i].getAttribute(attr)
+		eleAttr = eleAttr + '?' + getRandomNum();
+		ele[i].setAttribute(attr, eleAttr)
+	}
+}
+
+// attr 세팅
+function setAttr(ele, attr, value) {
+	ele.forEach(function(ele) {
+		ele.setAttribute(attr, value)
+	})
+}
+
+// url파라미터 값 구하기
+function getUrlParam(param) {
+	var urlParams = new URL(location.href).searchParams;
+	var name = urlParams.get(param);
+	return name;
+}
+
+// 가장 가까운 부모 찾기
+function closest(ele, selector) {
+	var matchesSelector = ele.matches || ele.webkitMatchesSelector || ele.mozMatchesSelector || ele.msMatchesSelector;
+	while (ele) {
+		if (matchesSelector.call(ele, selector)) {
+			return ele;
+		} else {
+			ele = ele.parentElement;
+		}
+	}
+	return null;
+}
+
+
+// 팝업
+function popOpen(ele, btn) {
+	var tar = document.querySelector(ele);
+	tar.setAttribute('tabindex', 0);
+	setTimeout(function() {
+		tar.focus();
+	},10)
+	tar.classList.add('pop-open');
+	btn.setAttribute('data-pop', tar.getAttribute('id'));
+	console.log(siblings(tar))
+	accessDisable(siblings(tar), 'nav');
+}
+
+function popClose(ele) {
+	var tar = document.querySelector(ele);
+	var btns = document.querySelectorAll('[data-pop]');
+	tar.removeAttribute('tabindex');
+	tar.classList.remove('pop-open');
+	accessEnable(siblings(tar), 'nav');
+	btns.forEach(function(ele) {
+		if(ele.dataset.pop === tar.getAttribute('id')) {
+			setTimeout(function() {
+				ele.focus();
+				ele.removeAttribute('data-pop');
+			},10)
+		}
+	})
+}
