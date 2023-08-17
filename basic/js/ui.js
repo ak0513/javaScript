55188d227205d2139f23b1b5bf7681a4cbcfac0c
ak0513 <minimar0812@gmail.com>
2023-08-17 13:45:00 +0900

    add

--- basic/js/ui.js
+++ basic/js/ui.js
@@ -6,6 +6,6 @@ if(url.indexOf('component') > -1) {
 document.write('<li' + 'nk rel="stylesheet" href="' + path + './css/common.css">\n'
 + '<li' + 'nk rel="stylesheet" href="' + path + './css/style.css">\n'
 + '<li' + 'nk rel="stylesheet" href="' + path + './css/highlight/atom-one-dark.min.css">\n'
-+ '<li' + 'nk rel="stylesheet" href="' + path + './css/highlight.css">\n'
++ '<li' + 'nk rel="stylesheet" href="' + path + './css/blog.css">\n'
 + '<scr' + 'ipt src="' + '' + path + './js/common.js"><\/scr' + 'ipt>\n'
 + '<scr' + 'ipt src="' + '' + path + './js/highlight.min.js"><\/scr' + 'ipt>\n');
