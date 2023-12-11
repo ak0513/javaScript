e1e517b22f59bf20eac210f660b42f0d25e39b3a
ak0513 <minimar0812@gmail.com>
2023-12-11 14:30:07 +0900

    gsap

--- tstory/css/test.js
+++ tstory/css/test.js
@@ -19,6 +19,28 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
     hljs.initHighlightingOnLoad();
+
+    /* 이미지 변환 */
+    // DOM에서 모든 p 태그를 선택합니다.
+    var paragraphs = document.querySelectorAll('p');
+
+    // 선택된 각 p 태그를 순회하면서 클래스가 없는 것을 골라냅니다.
+    for (var i = 0; i < paragraphs.length; i++) {
+        var paragraph = paragraphs[i];
+        // 클래스가 없는 경우에 대한 조건을 확인합니다.
+        if (paragraph.classList.length === 0 && !!paragraph.innerText.match('##_Image')) {
+            var innerTxt = paragraph.innerText
+            var imgUrl = innerTxt.split('|')[1].split('@')[1];
+            var caption = innerTxt.split('|')[4].split(',')[3].split('"')[3];
+            paragraph.innerHTML = `<figure class="imageblock"><span><img src="https://blog.kakaocdn.net/dn/${imgUrl}" alt=""></span><figcaption>${caption}</figcaption></figure>`;
+
+            while (paragraph.firstChild) {
+                paragraph.parentNode.insertBefore(paragraph.firstChild, paragraph);
+            }
+            // p 태그를 제거합니다.
+            paragraph.parentNode.removeChild(paragraph);
+        }
+    }
 })
 
 /* 샘플 버튼 제어 */
@@ -32,3 +54,5 @@ btnSample.forEach(function(item) {
         item.classList.add('on');
     })
 })
+
+
